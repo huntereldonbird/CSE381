e22b7ee84eb12b65cae10208b8853cc286b58cc8
Eldon <Birdheldon@gmail.com>
2025-03-03 16:30:05 -0700

    algorithms?

--- Assignments/AlgorithmLib/DijkstraShortestPath.cs
+++ Assignments/AlgorithmLib/DijkstraShortestPath.cs
@@ -24,6 +24,48 @@ public static class DijkstraShortestPath
      */
     public static (List<int>, List<int>) ShortestPath(Graph g, int startVertex)
     {
-        return (new List<int>(), new List<int>());
-    } 
+        int size = g.Size();
+        List<int> distance = Enumerable.Repeat(Graph.INF, size).ToList();
+        List<int> predecessor = Enumerable.Repeat(-1, size).ToList();
+        List<bool> visited = new List<bool>(new bool[size]);
+
+        PriorityQueue<int, int> pq = new PriorityQueue<int, int>();
+
+        pq.Enqueue(startVertex, 0);
+        distance[startVertex] = 0;
+        predecessor[startVertex] = Graph.INF;
+
+
+        while (pq.Count > 0) {
+
+            int current = pq.Dequeue();
+
+            if (visited[current]) {
+                continue;
+            }
+            visited[current] = true;
+
+            foreach (var edge in g.Edges(current)) {
+
+                int new_distance = distance[current] + edge.Weight;
+
+                if (visited[edge.DestId]) {
+                    continue;
+                }
+
+
+                if (new_distance < distance[edge.DestId]) {
+
+                    distance[edge.DestId] = new_distance;
+                    predecessor[edge.DestId] = current;
+
+                    pq.Enqueue(edge.DestId, new_distance);
+
+                }
+            }
+        }
+
+        return (distance, predecessor);
+    }
+
 }
--- Assignments/AlgorithmLib/StringMatcher.cs
+++ Assignments/AlgorithmLib/StringMatcher.cs
@@ -21,9 +21,59 @@ public static class StringMatcher
      *  Outputs:
      *     list of indices where the pattern matched (last char of pattern match)
      */
-    public static List<int> Match(string text,  string pattern, List<char> inputs)
-    {
-        return new List<int>();
+    public static List<int> Match(string text,  string pattern, List<char> inputs) {
+
+        // the book was really dense so I watched a video about a similar algorithm on youtube.
+
+        List<int> matched = new List<int>();
+        List<Dictionary<char, int>> builtalbe = BuildTable(pattern, inputs);
+
+        int state = 0; // this is that counting that you were talking about in class...
+
+
+        // so in short, we are couting up by each char, and if we find a matching char in our pattern we then incremenet the counter.
+        // then if the counter is the same size as the pattern length then we know we have found a solution, then we set it to a reset state
+
+        for (int i = 0; i < text.Length; i++) {
+
+            char ch = text[i];
+
+            if (builtalbe[state].ContainsKey(ch)) {
+                state = builtalbe[state][ch];
+            }
+            else {
+
+                // if we dont find it the counter needs to be reset.
+                state = 0;
+
+                if (builtalbe[state].ContainsKey(ch)) {
+                    state = builtalbe[state][ch];
+                }
+            }
+
+            if (state == pattern.Length) {
+
+                // if we have a completed word, append it to the output list
+
+                matched.Add(i);
+
+                if (builtalbe.Count > pattern.Length && builtalbe[pattern.Length].ContainsKey(ch)) {
+
+                    // basically, if there is still a chance that we could find another, we need to keep looking...
+
+                    state = builtalbe[pattern.Length][ch];
+                }
+                else {
+
+                    // ... if not, we need to restart completly
+                    state = 0;
+                }
+            }
+
+
+        }
+
+        return matched;
     }
 
     /* Build the Finite State Machine table for the pattern and list of valid
@@ -38,8 +88,54 @@ public static class StringMatcher
      *     dictionary shows the next state to goto for each of the valid
      *     inputs that can occur.
      */
-    public static List<Dictionary<char, int>> BuildTable(string pattern, List<char> inputs)
-    {
-        return new List<Dictionary<char, int>>();
+    public static List<Dictionary<char, int>> BuildTable(string pattern, List<char> inputs) {
+
+
+        // start the fms table
+        List<Dictionary<char, int>> fsm_talbe = new List<Dictionary<char, int>>();
+
+
+        // create the entries and keys
+        for (int i = 0; i <= pattern.Length; i++) {
+
+            fsm_talbe.Add(new Dictionary<char, int>());
+
+        }
+
+
+        // this is basically a 3d array, but with a dictionary so that we can go by char instead of by ints
+
+        // IM SO MAD, I HAD IT AS < FOR SO LONG AND IT TOOK ME SO LONG TO FIGURE IT OUT!!!!
+        for (int i = 0; i <= pattern.Length; i++) {
+
+
+
+            foreach (char _this in inputs) {
+
+                string ard = pattern.Substring(0, i) + _this; // This line I had to get help from the internet... I found that this was the easiest way to figure this out. ard means nothing i just pressed keys on keybaord
+                int next = 0;
+                for (int j = Math.Min(pattern.Length, ard.Length); j > 0; j--) {
+
+                    if (pattern.Substring(0, j) == ard.Substring(ard.Length - j)) {
+                        next = j;
+                        break;
+                    }
+                }
+
+                // add it all to the table
+
+                fsm_talbe[i][_this] = next;
+
+            }
+
+        }
+
+        return fsm_talbe;
+
+
+
+
+
+
     }
 }
